9a9cd376bbfe66410682d6d85c3b40cb80780109
Арина Новгородова <arrrishanov@gmail.com>
2025-09-09 12:57:13 +0300

    add HhAPI class

---
+++ src/API.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+import requests
+
+
+class BasicAPI(ABC):
+    """Базовый класс для работы с API сервиса с вакансиями"""
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def load_vacancies(self, keyword):
+        pass
+
+class HhAPI(BasicAPI):
+    """
+    Класс для работы с API HeadHunter
+    Класс Parser является родительским классом, который вам необходимо реализовать
+    """
+
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+        super().__init__()
+
+    def load_vacancies(self, keyword):
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
