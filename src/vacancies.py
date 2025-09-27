0b9c2700e244c6ab974d84e63a2d63e8779a500a
Арина Новгородова <arrrishanov@gmail.com>
2025-09-27 14:22:45 +0300

    add validation in Vacancy class

--- src/vacancies.py
+++ src/vacancies.py
@@ -2,12 +2,20 @@ class Vacancy:
     """Класс для вакансий"""
 
     def __init__(self, name: str, link: str, description: str, salary_from: int, salary_to: int, address: str):
-        self.name = name
+        self.name = name,
         self.link = link
         self.description = description
         self.salary_from = salary_from
         self.salary_to = salary_to
         self.address = address
+
+        # Валедации названия вакансии
+        if not isinstance(self.name, str) or len(self.name) == 0:
+            raise ValueError("Название вакансии должно быть непустой строкой")
+
+        # Валедации ссылки на вакансию
+        if not isinstance(self.link, str) or len(self.link) == 0:
+            raise ValueError("Ссылка на вакансию должно быть непустой строкой")
 
     def __str__(self):
         return f"Вакансия {self.name}\n{self.link}\nЗарплата {self.salary_from}-{self.salary_to} руб."
